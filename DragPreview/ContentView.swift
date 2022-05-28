dcb4118b0ae9edbe9a23dc0d78dc12dbe8b5f397
Guillaume Leclerc <guill.bout@gmail.com>
2022-05-28 15:31:18 +0800

    first commit

--- DragPreview/ContentView.swift
+++ DragPreview/ContentView.swift
@@ -1,21 +1,60 @@
-//
-//  ContentView.swift
-//  DragPreview
-//
-//  Created by Guillaume Leclerc on 28/05/2022.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+struct AppDetail {
+    
+    let bundleIdentifier: String
+    let icon: NSImage
+    
+    init(bundleIdentifier: String) {
+        self.bundleIdentifier = bundleIdentifier
+        
+        if
+            let url = NSWorkspace.shared.urlForApplication(withBundleIdentifier: bundleIdentifier),
+            let representation = NSWorkspace.shared.icon(forFile: url.path).bestRepresentation(for: NSRect(x: 0, y: 0, width: 64, height: 64), context: nil, hints: nil)
+        {
+            self.icon = NSImage(size: representation.size)
+            self.icon.addRepresentation(representation)
+        } else {
+            self.icon = NSApp.applicationIconImage!
+        }
     }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct ContentView: View {
+
+    @State private var apps: [AppDetail] = [
+        AppDetail(bundleIdentifier: "com.apple.Mail"),
+        AppDetail(bundleIdentifier: "com.apple.MobileSMS"),
+        AppDetail(bundleIdentifier: "com.apple.Safari"),
+    ]
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Image(nsImage: apps.first == nil ? NSApp.applicationIconImage! : apps.first!.icon)
+                .onDrag {
+                    guard
+                        let app = apps.first,
+                        let url = NSWorkspace.shared.urlForApplication(withBundleIdentifier: app.bundleIdentifier)
+                    else {
+                        return NSItemProvider()
+                    }
+                                       
+                    return NSItemProvider(object: url as NSURL)
+                } preview: {
+                    Text(apps.first == nil ? "no app" : apps.first!.bundleIdentifier)
+                    Image(nsImage: apps.first == nil ? NSApp.applicationIconImage! : apps.first!.icon)
+                }
+            Button("next") {
+                apps.removeFirst()
+            }
+            .disabled(apps.isEmpty)
+        }
+        .frame(width: 200, height: 200)
+        .padding()
+        
     }
+
 }
