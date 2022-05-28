251338ee70fc9f8c50c5d6a64f781b0ac7d5de14
Guillaume Leclerc <guill.bout@gmail.com>
2022-05-28 15:07:26 +0800

    Initial Commit

---
+++ DragPreview/DragPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragPreviewApp.swift
+//  DragPreview
+//
+//  Created by Guillaume Leclerc on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DragPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
